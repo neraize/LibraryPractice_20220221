2f3a7e3e4dbc5861265c79b3933a08c80694ca7a
seojungmin <neraize@gmail.com>
2022-02-21 12:12:30 +0900

    CALL 기능 설명 -권한 문제로 앱이 죽는다.

--- app/src/main/java/com/nepplus/librarypractice_20220221/MainActivity.kt
+++ app/src/main/java/com/nepplus/librarypractice_20220221/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.nepplus.librarypractice_20220221
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -19,11 +20,18 @@ class MainActivity : AppCompatActivity() {
 
         // 버튼 이외의, TextView, ImageView, LinearLayout 등등도  setOnClickListener로 이벤트 처리 가능함
         imgProfile.setOnClickListener {
-
             // 사진을 크게보는 액티비티로 이동
             val myIntent =  Intent(this, ViewPhotoActivity::class.java)
             startActivity(myIntent)
+        }
 
+
+        btnCall.setOnClickListener {
+
+            //
+            val myUri = Uri.parse("tel:010-1234-5678")
+            val myIntent =  Intent(Intent.ACTION_CALL, myUri)
+            startActivity(myIntent)
         }
     }
 
